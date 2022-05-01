3e0261339506ed592d5a1793092f4eb4482200bb
Junaid Razaq Casual <junaidrazaqcasual@Junaids-MacBook-Pro.local>
2022-05-01 20:34:06 +0100

    added_shadow_styles

---
+++ src/_Shadow/index.js
@@ -0,0 +1,98 @@
+export const shadowVeryLight = {
+  shadowColor: '#000',
+  shadowOffset: {
+    width: 0,
+    height: 2,
+  },
+  shadowOpacity: 0.1,
+  shadowRadius: 4.41,
+  elevation: 1,
+};
+
+export const shadowVeryLightLow = {
+  shadowColor: '#000',
+  shadowOffset: {
+    width: 0,
+    height: 6,
+  },
+  shadowOpacity: 0.12,
+  shadowRadius: 3.41,
+  elevation: 2,
+};
+
+export const shadowAround = {
+  shadowColor: '#000',
+  shadowOffset: {
+    width: 0,
+    height: 0,
+  },
+  shadowOpacity: 0.2,
+  shadowRadius: 5.41,
+  elevation: 2,
+};
+
+export const shadowLight = {
+  shadowColor: '#000',
+  shadowOffset: {
+    width: 0,
+    height: 3,
+  },
+  shadowOpacity: 0.35,
+  shadowRadius: 4.41,
+  elevation: 10,
+};
+
+export const shadowMedium = {
+  shadowColor: '#000',
+  shadowOffset: {
+    width: 0,
+    height: 3,
+  },
+  shadowOpacity: 0.5,
+  shadowRadius: 4.41,
+  elevation: 6,
+};
+
+export const shadowDark = {
+  shadowColor: '#000',
+  shadowOffset: {
+    width: 0,
+    height: 3,
+  },
+  shadowOpacity: 0.8,
+  shadowRadius: 4.41,
+  elevation: 10,
+};
+
+export const shadowDarkHigh = {
+  shadowColor: '#000',
+  shadowOffset: {
+    width: 0,
+    height: -10,
+  },
+  shadowOpacity: 0.2,
+  shadowRadius: 14.41,
+  elevation: 10,
+};
+
+export const bottomShadow = {
+  shadowColor: '#222',
+  shadowOffset: {
+    width: 0,
+    height: 4,
+  },
+  shadowOpacity: 0.1,
+  shadowRadius: 3.41,
+  elevation: 2,
+};
+
+export const topShadow = {
+  shadowColor: '#222',
+  shadowOffset: {
+    width: 0,
+    height: -4,
+  },
+  shadowOpacity: 0.1,
+  shadowRadius: 3.41,
+  elevation: 2,
+};
