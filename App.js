c5aee3c7acba0a3834dc463adcf4680ec55af690
Junaid Razaq Casual <junaidrazaqcasual@Junaids-MacBook-Pro.local>
2022-05-03 22:04:58 +0100

    code_cleaned

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-import Root from './src';
 
 // REDUX
 import {Provider} from 'react-redux';
+import Root from './src';
 import ConfigureStore from './src/redux/ConfigureStore';
+
 const store = ConfigureStore();
 
 const App = () => {
