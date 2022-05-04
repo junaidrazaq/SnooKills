656197f277e320562af86d0f9d293e9a411746fc
Junaid Razaq Casual <junaidrazaqcasual@Junaids-MacBook-Pro.local>
2022-05-04 20:53:40 +0100

    added_insets_top_padding

--- src/common/HeaderWithBack.js
+++ src/common/HeaderWithBack.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import {useNavigation} from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/AntDesign';
 import {Pressable, Text, View} from '.';
+import Icon from 'react-native-vector-icons/AntDesign';
+import {useNavigation} from '@react-navigation/native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 const CustomHeaderWithBack = ({text}) => {
   const navigation = useNavigation();
+  const insets = useSafeAreaInsets();
 
   // RENDER
   return (
-    <View horizontal paddingHorizontal={20} alignItems="center">
+    <View
+      horizontal
+      paddingHorizontal={20}
+      alignItems="center"
+      style={{paddingTop: insets.top + 20}}>
       <Pressable onPress={() => navigation.goBack()}>
         <Icon name="arrowleft" size={30} />
       </Pressable>
