0de51ce2674310d965e7de41810a009211ef38f4
Junaid Razaq Casual <junaidrazaqcasual@Junaids-MacBook-Pro.local>
2022-05-04 21:16:43 +0100

    off_kills_when_minusing_life

--- src/home/redux/reducer.js
+++ src/home/redux/reducer.js
@@ -39,14 +39,20 @@ export default function reducer(state = INITIAL_STATE, action) {
         ...state,
         yellowLives:
           payload === 'yellow' ? state.yellowLives - 1 : state.yellowLives,
+        yellowKills: payload === 'yellow' ? false : state.yellowKills,
         greenLives:
           payload === 'green' ? state.greenLives - 1 : state.greenLives,
+        greenKills: payload === 'green' ? false : state.greenKills,
         brownLives:
           payload === 'brown' ? state.brownLives - 1 : state.brownLives,
+        brownKills: payload === 'brown' ? false : state.brownKills,
         blueLives: payload === 'blue' ? state.blueLives - 1 : state.blueLives,
+        blueKills: payload === 'blue' ? false : state.blueKills,
         pinkLives: payload === 'pink' ? state.pinkLives - 1 : state.pinkLives,
+        pinkKills: payload === 'pink' ? false : state.pinkKills,
         blackLives:
           payload === 'black' ? state.blackLives - 1 : state.blackLives,
+        blackKills: payload === 'black' ? false : state.blackKills,
       };
 
     case constants.TOGGLE_KILLS.SUCCESS:
