956489d9cc6df56bcbab4f023f3ddd7c4e493fe9
Junaid Razaq Casual <junaidrazaqcasual@Junaids-MacBook-Pro.local>
2022-06-09 07:13:43 +0100

    added_loading_indicator_component

--- src/common/index.js
+++ src/common/index.js
@@ -3,3 +3,4 @@ export {default as Text} from './Text';
 export {default as Pressable} from './Pressable';
 export {default as HeaderWithBack} from './HeaderWithBack';
 export {default as DropDownPicker} from './DropDownPicker';
+export {default as LoadingIndicator} from './LoadingIndicator';
--- src/home/components/index.js
+++ src/home/components/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import {HeaderWithBack, View} from '../../common';
+import React, {useEffect} from 'react';
+import {HeaderWithBack, LoadingIndicator, View} from '../../common';
 import AllPlayers from './AllPlayers';
 import ResetLives from './ResetLives';
+import {InteractionManager} from 'react-native';
 
 const Home = () => {
   const containerStyle = {flex: 1};
+  const [loading, setLoading] = React.useState(true);
+
+  useEffect(() => {
+    InteractionManager.runAfterInteractions(() => {
+      setLoading(false);
+    });
+  }, []);
 
   // RENDER
   return (
     <View style={containerStyle}>
       <HeaderWithBack text="Home" />
-      <AllPlayers />
-      <ResetLives />
+      {loading ? (
+        <LoadingIndicator containerStyles={{bottom: 20}} />
+      ) : (
+        <>
+          <AllPlayers />
+          <ResetLives />
+        </>
+      )}
     </View>
   );
 };
