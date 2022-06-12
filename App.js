1105f45cb811a09a7abbf8d83e945d086b1df664
Junaid Razaq Casual <junaidrazaqcasual@Junaids-MacBook-Pro.local>
2022-06-12 15:11:01 +0100

    configure_redux_toolkit_and_added_console_polyfill

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import 'react-native-console-time-polyfill';
 
 // REDUX
 import {Provider} from 'react-redux';
 import Root from './src';
-import ConfigureStore from './src/redux/ConfigureStore';
-
-const store = ConfigureStore();
+import {store} from './src/redux/ConfigureStore';
 
 const App = () => {
   return (
